a1e9f52ed301ff4e4a1b47162e2e154b831a71ab
Lorenzo Dinardo <lore@MBPdiLorenzo3.home-life.hub>
2022-08-22 17:13:56 +0200

    Aggiunta classe transazione

---
+++ Transazione.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Lorenzo Dinardo on 22/08/22.
+//
+#include "Transazione.h"
+#include <string>
+
+Transazione::Transazione(std::string destination_address, std::string source_address, float amount,
+                         std::string transation_ID, Transazione::transation_type type, std::string timestamp) {
+    this->destination_address = destination_address;
+    this->source_address = source_address;
+    this->amount = amount;
+    this->transation_ID= transation_ID;
+    this->type = type;
+    this->timestamp = timestamp;
+}
+
+std::string Transazione::getSourceAddress() {
+    return this->source_address;
+}
+std::string Transazione::getDestinationAddress() {
+    return this-> destination_address;
+}
+float Transazione::getAmount() const {
+    return this->amount;
+}
+std::string Transazione::getTransationID() {
+    return this->transation_ID;
+}
+
+TransationType Transazione::getTransationType() {
+    return this->type;
+}
+std::string Transazione::getTimestamp() {
+    return this->timestamp;
+}
---
+++ Transazione.h
@@ -0,0 +1,29 @@
+//
+// Created by Lorenzo Dinardo on 22/08/22.
+//
+#include <string>
+
+#ifndef LABORATORIOPROG_LORENZODINARDO_TRANSAZIONE_H
+#define LABORATORIOPROG_LORENZODINARDO_TRANSAZIONE_H
+enum TransationType { addition, removal };
+
+class Transazione{
+private:
+    std::string destination_address;
+    std::string source_address;
+    float amount;
+    std::string transation_ID;
+    TransationType type;
+    std::string timestamp;
+public:
+    Transazione(std::string destination_address, std::string source_address, float amount, std::string transation_ID, TransationType type, std::string timestamp );
+    std::string getSourceAddress();
+    std::string getDestinationAddress();
+    float getAmount() const;
+    std::string getTransationID();
+    TransationType getTransationType();
+    std::string getTimestamp();
+
+
+};
+#endif //LABORATORIOPROG_LORENZODINARDO_TRANSAZIONE_H
